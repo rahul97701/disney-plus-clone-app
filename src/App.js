729a25d01afe25f0058e88db2b1cad53e7c2472e
rahul97701 <singh.rahulsingh.om07@gmail.com>
2024-10-16 13:34:08 -0400

    ADD: app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from './app/components/Header';
-import Home from './app/components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./app/components/Header";
+import Home from "./app/components/Home";
+import Detail from "./app/components/Detail";
+import Login from "./app/components/Login";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    <Home />
+      <Header />
+      
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
     </div>
   );
 }
